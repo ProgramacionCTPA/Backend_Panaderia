bce121c60798a7e2b204f09662877900543e016e
ProgramacionCTPA <160040652+ProgramacionCTPA@users.noreply.github.com>
2025-11-06 22:52:28 -0600

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+
 const productSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: String,
   price: { type: Number, required: true },
   category: { type: String, required: true },
-  image: String,
+  image: String, // Aquí guardaremos el base64
   stock: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now }
 });
+
 module.exports = mongoose.model('Product', productSchema);
